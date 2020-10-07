e9e3d8cc73c77ac334c535062353ce598cc6be93
rashevskyv <rashevsky.v@gmail.com>
2020-10-07 21:19:55 +0300

    pure 14.4 with kefir version on settings

--- stratosphere/loader/source/ldr_meta.cpp
+++ stratosphere/loader/source/ldr_meta.cpp
@@ -123,7 +123,7 @@ namespace ams::ldr {
             const bool is_signature_valid = crypto::VerifyRsa2048PssSha256(sig, sig_size, mod, mod_size, exp, exp_size, msg, msg_size);
             R_UNLESS(is_signature_valid || !IsEnabledProgramVerification(), ResultInvalidAcidSignature());
 
-            meta->is_signed = false;
+            meta->check_verification_data = false;
             return ResultSuccess();
         }
 
